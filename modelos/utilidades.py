5c691a71ca5594462bcf36a0f45fc2cbeb66b169
Roberto <betetinho_@hotmail.com>
2025-10-26 16:10:05 -0300

    Adicionando comentário a utilidades

--- modelos/utilidades.py
+++ modelos/utilidades.py
@@ -2,5 +2,6 @@ import time
 import os
 
 def limpa():
+    # Realizando a leitura do terminal após rodar o projeto app.py. Após 10s, o terminal será limpo.
         time.sleep(10)
         os.system("clear")
